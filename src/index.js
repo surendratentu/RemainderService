f7a9a785645d965a4d1f47e624977f15143bf6ac
surendratentu <surendratentu2005@gmail.com>
2026-01-01 15:19:19 +0530

    Setup of smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -3,14 +3,23 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
+const { sendBasicEmail } = require('./services/email-service');
+
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.listen(PORT, () => {
-        console.log(`Server started at ${PORT}`);
-    })
-}
+        console.log(`Server started at port ${PORT}`);
+
+        sendBasicEmail(
+            'support@admin.com',
+            'surendratentu2005@gmail.com',
+            'This is a testing email',
+            'Hey, how are you, I hope you like the support'
+        );
+    });
+};
 
 setupAndStartServer();
